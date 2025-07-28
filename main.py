e265b61e481f9d2216bf0c2a058717f5d11f14c1
New Reno <karevanton@hotmail.com>
2025-07-28 22:46:02 +0300

    Add def download_image

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from pathlib import Path
 import os
 
 
-def main():
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+def download_image(url):
+    # url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
     base_path = Path(__file__).absolute().parent
     output_dir = base_path / "images"
     filename = output_dir / "hubble.jpeg"
     os.makedirs(output_dir, exist_ok=True)
-    try:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
-        }
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
 
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-    
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    try:
+        url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+        download_image(url)
     except requests.exceptions.HTTPError as err:
         print(f"HTTP ошибка: {err}")
         exit()
